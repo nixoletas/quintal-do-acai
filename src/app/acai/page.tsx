679121021219991cab92b6036860a2780858b133
Nicholas_Miyasato <nicholetas@gmail.com>
2024-05-20 17:31:38 -0400

    remove hero

--- src/app/acai/page.tsx
+++ src/app/acai/page.tsx
@@ -1,12 +1,10 @@
 import AcaiMenu from "@/components/AcaiMenu";
-import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Navbottom from "@/components/Navbottom";
 
 export default function Acai() {
   return (
     <main>
-        <Hero />
       <Navbar />
       <AcaiMenu />
       <Navbottom />
