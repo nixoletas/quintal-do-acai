fcc06c9bc87101336c9903694dfc2d0468749824
Nicholas_Miyasato <nicholetas@gmail.com>
2024-05-20 18:26:33 -0400

    link iconssdsd

--- src/app/bebidas/page.tsx
+++ src/app/bebidas/page.tsx
@@ -1,12 +1,10 @@
 import Navbar from "@/components/Navbar";
 import BebidasMenu from "@/components/BebidasMenu";
 import Navbottom from "@/components/Navbottom";
-import Hero from "@/components/Hero";
 
 export default function Bebidas() {
   return (
     <main>
-        <Hero />
       <Navbar />
       <BebidasMenu />
       <Navbottom />
